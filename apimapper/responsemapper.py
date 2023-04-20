4f73fd189486ae76a849b744417d922f8ac76458
gythaogg <gythaoggscat@gmail.com>
2023-04-20 09:12:18 +0200

    Precommit formatting

--- apimapper/responsemapper.py
+++ apimapper/responsemapper.py
@@ -13,11 +13,11 @@ class ResponseMapper:
         return
 
     def _is_relevant_result(self, res_item):
-        '''
+        """
         called for every item in the response (res_item)
         returns True if the response matches the "filter" condition specified in the configurationq
 
-        '''
+        """
         for filter_field, filter_value in self.mapping.get(FILTER, {}).items():
             if res_item.get(filter_field) != filter_value:
                 return False
@@ -29,7 +29,7 @@ class ResponseMapper:
         # Rule based mapping
         mapped_schema = {}
         for to_key, rule_data in self.mapping.get(RULES, {}).items():
-            logger.debug('Mapping rule: %s <- %s', to_key, rule_data)
+            logger.debug("Mapping rule: %s <- %s", to_key, rule_data)
             rule = rule_data.get(RULE)
             fields = rule_data.get(FIELDS, {})
             eval_params = {}
@@ -38,17 +38,20 @@ class ResponseMapper:
             for k, v in fields.items():
                 eval_params[k] = self._deep_search(res_item, v)
                 # res_item.get(v, '')
-                logging.debug('%s: %s', k, rule.format(**eval_params))
+                logging.debug("%s: %s", k, rule.format(**eval_params))
 
             try:
                 mapped_schema[to_key] = eval(rule.format(**eval_params))
-                logging.debug('%s: %s', to_key, mapped_schema[to_key])
+                logging.debug("%s: %s", to_key, mapped_schema[to_key])
             except Exception as e:
                 # Bad rule: keep calm and carry on
-                mapped_schema[to_key] = ''
+                mapped_schema[to_key] = ""
                 logging.warning(
-                    'Encountered bad rule (%s) for input "%s"\n %s', rule_data,
-                    eval_params, repr(e))
+                    'Encountered bad rule (%s) for input "%s"\n %s',
+                    rule_data,
+                    eval_params,
+                    repr(e),
+                )
 
         return mapped_schema
 
@@ -61,12 +64,11 @@ class ResponseMapper:
 
     @staticmethod
     def _deep_search(res_item, from_key_path):
-        '''
+        """
         maps the parmeter empedded within JSON fields
-        '''
+        """
 
-        if not isinstance(from_key_path, str) and isinstance(
-                from_key_path, Iterable):
+        if not isinstance(from_key_path, str) and isinstance(from_key_path, Iterable):
             deep_search = deepcopy(res_item)
             for from_key_item in from_key_path:
                 deep_search = deep_search.get(from_key_item)
@@ -86,25 +88,25 @@ class ResponseMapper:
         return {}
 
     def get_mapped_response(self, original_response):
-        '''
+        """
         map original response to fields specified in config
-        '''
+        """
         if not self.mapping:
-            logging.warning('No mapping found. Returning as-is.')
+            logging.warning("No mapping found. Returning as-is.")
             return original_response
 
         mapped_response = []
         if self.mapping.get(RESULT):
-            original_response = original_response.get(
-                self.mapping.get(RESULT), [])
+            original_response = original_response.get(self.mapping.get(RESULT), [])
 
         try:
             for res_item in original_response:  # what
                 mapped_item = self._map_item(res_item)
-                if len(mapped_item): mapped_response.append(mapped_item)
+                if len(mapped_item):
+                    mapped_response.append(mapped_item)
 
             return mapped_response
         except TypeError as te:
-            logging.warning('Encountered TypeError: %s', repr(te))
+            logging.warning("Encountered TypeError: %s", repr(te))
 
         return []
